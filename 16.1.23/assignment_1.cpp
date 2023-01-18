3179b3209cf1945783f54341387872f05be8662d
kabirsingh28 <7darkshades@gmail.com>
2023-01-19 00:31:11 +0530

    finished assignment 2 of linked list

--- 16.1.23/assignment_1.cpp
+++ 16.1.23/assignment_1.cpp
@@ -24,54 +24,54 @@ node* take_input(){
 }
 
 int get_index(int x, node * head){
-    node * temp = head;
+    node * t1 = head;
     int count = 0;
-    while(temp->data!=x&&temp->next!=NULL){
-        temp=temp->next;
+    while(t1->data!=x&&t1->next!=NULL){
+        t1=t1->next;
         count++;
     }
-    if(temp->next==NULL){
+    if(t1->next==NULL){
     return -1;
     }
     return count;
 }
 
 void print(node * head){
-    node * temp = head;
-    while(temp!=NULL){
-        cout<<temp->data<<" ";
-        temp=temp->next;
+    node * t1 = head;
+    while(t1!=NULL){
+        cout<<t1->data<<" ";
+        t1=t1->next;
     }
     cout<<endl;
 }
 
 int size(node * head){
     int count = 0;
-    node * temp = head;
-    while(temp!=NULL){
-       temp=temp->next;
+    node * t1 = head;
+    while(t1!=NULL){
+       t1=t1->next;
        count++;
     }
     return count;
 }
 
 node* AppendLastNtoFirst(int x,node * head){
-   node * temp = head;
+   node * t1 = head;
    int count = 0;
    if(size(head)==x){
     return head;
    }
    while(count!=(size(head)-x-1)){
-    temp=temp->next;
+    t1=t1->next;
     count++;
    }
-   node * temp2 = temp->next;
+   node * temp2 = t1->next;
    while(temp2->next!=NULL){
     temp2=temp2->next;
    }
    temp2->next = head;
-   head = temp-> next;
-   temp->next = NULL;
+   head = t1-> next;
+   t1->next = NULL;
    return head;
 }
 
@@ -96,18 +96,18 @@ void Eliminate_duplicate(node * head){
 
 //  void print_reverse(node * head){
 //   int t = 1;
-//   node * temp = head;
+//   node * t1 = head;
 //   while(t!=size(head)){
      
 //      int k = size(head)-t;
 //      while(k--){
-//         temp=temp->next;
+//         t1=t1->next;
 //      }
-//      cout<<temp->data<<" ";
+//      cout<<t1->data<<" ";
 //      t++;
-//      temp = head;
+//      t1 = head;
 //   }
-//   cout<<temp->data<<" ";
+//   cout<<t1->data<<" ";
 // }
 // above code is correct but time complexity is high i.e n^2
 
@@ -119,6 +119,39 @@ void print_reverse(node * head){
     cout<<head->data<<" ";
 }
 
+node* reverse_ll(node * head){
+    if(!head || !(head->next)) return head;
+    node * temp = reverse_ll(head->next);//called recursion on the rest of the linked list expcept the first node
+    head->next->next = head; // attached the first node at the last,node 2 is common to temp and head
+    head->next = NULL;
+    return temp;
+}
+
+int compare_ll(node * t1, node * t2){
+    int flag = 0;
+    while(t1!=NULL){
+     if(t1->data!=t2->data){
+        flag++;
+     }
+     t1=t1->next;
+     t2=t2->next;
+    }
+    if(flag==0) return 1;
+    else return 0;
+}
+
+int CheckPalindrome(node * head){
+    node * t1 = head;
+    int k = size(t1)/2 - 1;
+    while(k--){
+       t1=t1->next;
+    }
+    node * t2 = t1->next;
+    t1->next=NULL;
+    t2 = reverse_ll(t2);
+    return compare_ll(head,t2);
+}
+
 int main()
 {   node * head = take_input();
     // int x;
@@ -126,6 +159,7 @@ int main()
     //cout<<get_index(x,head);
     //head = AppendLastNtoFirst(x,head);
     //Eliminate_duplicate(head);
-    print_reverse(head);
+    //print_reverse(head);
+    cout<<CheckPalindrome(head);
     return 0;
 }
