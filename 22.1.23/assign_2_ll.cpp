9d436042dec3ef3539a2234d6f8acd36de69f9eb
kabirsingh28 <7darkshades@gmail.com>
2023-01-24 00:30:04 +0530

    completed assignment 2 of linked list

--- 22.1.23/assign_2_ll.cpp
+++ 22.1.23/assign_2_ll.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 #include "NodeClass.cpp"
 
+class Pair{
+    public:
+    node * head;
+    node * tail;
+};
+
 node* take_input(){
     int data;
     cin>>data;
@@ -102,12 +108,152 @@ node* even_odd(node * head){
     
 }
 
+node* delete_MN(int n, int m, node * head){
+    node * temp = head;
+    if(n==0){
+        return NULL;
+    }
+    while(1){
+        int a=n-1,b=m+1;
+        while(a>0){
+            temp=temp->next;
+            if(temp==NULL) return head;
+            a--;
+        }
+        node * p = temp->next;
+        while(b--){
+            temp->next = p;
+            if(p==NULL) return head;
+            p=p->next;
+            delete temp->next;
+        }
+        temp=temp->next;
+    }
+}
+
+node* swap_ij(int i, int j, node * head){
+    node * temp = head;
+    int count = 0;
+    if(i==0){
+        node * c1 = head;
+        while(count!=j-1){
+         temp=temp->next;
+         count++;
+        }
+        node * p2 = temp, * c2 = temp->next;
+        node * t = c2->next;
+        c2->next=c1->next;
+        c1->next=t;
+        p2->next=c1;
+        return c2;
+
+    }
+    while(count!=i-1){
+        temp=temp->next;
+        count++;
+    }
+    node * p1 = temp, * c1 = temp->next;
+    while(count!=j-1){
+        temp=temp->next;
+        count++;
+    }
+    node * p2 = temp, * c2 = temp->next;
+    p1->next=c2;
+    p2->next=c1;
+    node * t = c2->next;
+    c2->next=c1->next;
+    c1->next=t; 
+
+    return head;
+}
+
+Pair reversell_better(node * head){
+    if(!head || !head->next){//if only 1 or no element is present
+       Pair ans;
+       ans.head=head;
+       ans.tail=head;
+       return ans;
+
+    }
+
+    Pair smallans = reversell_better(head->next); //head and tail of samll reversed ll gets saved in smallans
+    smallans.tail->next=head;
+    head->next=NULL;
+    Pair ans;
+    ans.head=smallans.head;
+    ans.tail=head;
+    return ans;
+}
+
+node* k_reverse(int k, node * head){
+    node * temp = head;
+    node * h1 = head;
+    node * t1 = head;
+    int count = 0;
+    if(k==0){
+        return head;
+    }
+    if(temp==NULL){
+        return NULL;
+    }
+    while(count!=k-1&&temp->next!=NULL){
+       temp=temp->next;
+       count++;
+    }
+    t1=temp;
+    node * h2 = t1->next;
+    t1->next = NULL;
+
+    Pair smallans = reversell_better(h1);
+    h1 = smallans.head;
+    t1 = smallans.tail;
+
+    t1->next=k_reverse(k,h2);
+    return h1;
+
+}
+
+int len(node * head){
+    node * temp = head;
+    int count=1;
+    while(temp!=NULL){
+        temp=temp->next;
+        count++;
+    }
+    return count;
+}
+
+node* bubble_sort(node * head){
+    node * a = head;
+    node * b = head->next;
+    int i = 0;
+    while(b!=NULL){
+   if(a->data>b->data){
+       head = swap_ij(i,i+1,);
+       print(head);
+   }
+       i++;
+       a=b;
+       b=b->next;
+   }
+
+return head;
+}
+
 int main()
 {   node * head = take_input();
     // int x;
     // cin>>x;
     // cout<<return_index(x,head);
-    head = even_odd(head);
+    //head = even_odd(head);
+    // int n,m;
+    // cin>>n>>m;
+    //head = delete_MN(n,m,head);
+    //head = swap_ij(n,m,head);
+    // int k;
+    // cin>>k;
+    // head = k_reverse(k,head);
+    head = bubble_sort(head);
     print(head);
     return 0;
 }
---
+++ 22.1.23/tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+ // if(i==0){
+    //   while(count!=j-1){
+    //     temp=temp->next;
+    //     count++;
+    // }
+    // node * h = NULL, * c1 = head;
+    // node * p = temp, * c2 = temp->next;
+    // p->next=c1;
+    // c1->next=c2->next;
+    
+
+    // }
