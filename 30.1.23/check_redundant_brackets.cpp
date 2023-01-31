151a756870f987830528c414d08d13243918a1fc
kabirsingh28 <7darkshades@gmail.com>
2023-01-31 15:00:21 +0530

    about to complete assignment on stacks and queues

--- 30.1.23/check_redundant_brackets.cpp
+++ 30.1.23/check_redundant_brackets.cpp
@@ -3,6 +3,29 @@ using namespace std;
 #include <stack>
 
 int main()
-{
+{   
+    stack<char> s;
+    int i=0,flag=0;
+    string str;
+    cin>>str;
+    while(i!=str.length()){
+        int count=0;
+        for(;str[i]!=')';i++){
+            s.push(str[i]);
+        }
+        i++;
+        while(s.top()!='('){
+            count++;
+            s.pop();
+        }
+        s.pop();
+        if(count==0||count==1){
+            cout<<"true";
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0) cout<<"false";
+
     return 0;
 }
