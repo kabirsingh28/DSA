424cbf4091a2462704aaeef59c23fdb0381c9fed
kabirsingh28 <7darkshades@gmail.com>
2023-03-06 15:31:05 +0530

    successfully coded count_nodes and prepared for day 73

--- march_2023/7.3.23/BT_6.cpp
+++ march_2023/7.3.23/BT_6.cpp
@@ -34,35 +34,8 @@ BinaryTreeNode<int>* takeinput_levelwise(){
    return root;
 }
 
-void print_levelwise(BinaryTreeNode<int> * root){
-    queue<BinaryTreeNode<int>*> pendingQueue;
-    pendingQueue.push(root);
-    while(!pendingQueue.empty()){
-        BinaryTreeNode<int> * front = pendingQueue.front();
-        pendingQueue.pop();
-        cout<<front->data<<": ";
-        if(front->left!=NULL){
-            cout<<"L"<<front->left->data<<",";
-            pendingQueue.push(front->left);
-        }
-        if(front->right!=NULL){
-            cout<<"R"<<front->right->data;
-            pendingQueue.push(front->right);
-        }
-        cout<<endl;
-    }
-}
-
-int count_nodes(BinaryTreeNode<int> * root){
-    if(root==NULL) return 0;
-    int ans=1;
-    ans+=count_nodes(root->left)+count_nodes(root->right);
-    return ans;
-}
 
 int main()
-{   BinaryTreeNode<int> * root = takeinput_levelwise();
-    //print_levelwise(root);
-    cout<<count_nodes(root);
-    return 0;
+{  BinaryTreeNode<int> * root = takeinput_levelwise();
+   return 0;
 }
