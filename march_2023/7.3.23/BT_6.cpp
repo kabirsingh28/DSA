0724ddb741a3b06dd625fa619797a33e7b9e1acb
kabirsingh28 <7darkshades@gmail.com>
2023-03-07 17:37:13 +0530

    successfully solved problems upto in_order traversal

--- march_2023/7.3.23/BT_6.cpp
+++ march_2023/7.3.23/BT_6.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 BinaryTreeNode<int>* takeinput_levelwise(){
    int rootData;
-   cout<<"Enter root data: ";
+   cout<<"Enter root data:"<<endl;
    cin>>rootData;
    if(rootData==-1) return NULL;
    BinaryTreeNode<int> * root = new BinaryTreeNode<int>(rootData);
@@ -13,7 +13,7 @@ BinaryTreeNode<int>* takeinput_levelwise(){
    while(!pendingQueue.empty()){
     BinaryTreeNode<int> * front = pendingQueue.front();
     pendingQueue.pop();
-    cout<<"Enter left child of "<<front->data<<": ";
+    cout<<"Enter left child of "<<front->data<<": "<<endl;
     int left_child;
     cin>>left_child;
     if(left_child!=-1){
@@ -21,7 +21,7 @@ BinaryTreeNode<int>* takeinput_levelwise(){
         front->left = leftChild;
         pendingQueue.push(leftChild);
     }
-    cout<<"Enter right child of "<<front->data<<": ";
+    cout<<"Enter right child of "<<front->data<<": "<<endl;
     int right_child;
     cin>>right_child;
     if(right_child!=-1){
@@ -34,8 +34,64 @@ BinaryTreeNode<int>* takeinput_levelwise(){
    return root;
 }
 
+bool is_node_present(BinaryTreeNode<int> * root, int x){
+    if(root==NULL) return false;
+    if(root->data==x || is_node_present(root->left,x) || is_node_present(root->right,x)) return true;
+    else return false;
+}
+
+int height(BinaryTreeNode<int> * root){
+    if(root==NULL) return 0;
+    int h = max(height(root->left),height(root->right)) + 1;
+    return h;
+}
+
+void print_levelwise(BinaryTreeNode<int> * root){
+    queue<BinaryTreeNode<int>*> pendingQueue;
+    pendingQueue.push(root);
+    while(!pendingQueue.empty()){
+        BinaryTreeNode<int> * front = pendingQueue.front();
+        pendingQueue.pop();
+        cout<<front->data<<": ";
+        if(front->left!=NULL){
+            cout<<"L"<<front->left->data<<",";
+            pendingQueue.push(front->left);
+        }
+        if(front->right!=NULL){
+            cout<<"R"<<front->right->data;
+            pendingQueue.push(front->right);
+        }
+        cout<<endl;
+    }
+}
+
+void mirror_bt(BinaryTreeNode<int> * root){
+    if(root==NULL) return;
+    BinaryTreeNode<int> * temp = root->left;
+    root->left = root->right;
+    root->right = temp;   
+    mirror_bt(root->left);
+    mirror_bt(root->right);
+}
+
+void in_order(BinaryTreeNode<int> * root){
+    if(root==NULL) return;
+    in_order(root->left);
+    cout<<root->data<<" ";
+    in_order(root->right);
+
+}
 
 int main()
 {  BinaryTreeNode<int> * root = takeinput_levelwise();
+//    print_levelwise(root);
+//    mirror_bt(root);
+//    print_levelwise(root);
+      in_order(root);
+//    int x;
+//    cin>>x;
+//    if(is_node_present(root,x)) cout<<"true";
+//    else cout<<"false";
+//    cout<<height(root);
    return 0;
 }
