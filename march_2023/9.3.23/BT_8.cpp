8eaf429d81ea8cc2b981f7802bedf952027c1098
kabirsingh28 <7darkshades@gmail.com>
2023-03-09 18:55:01 +0530

    about to complete lecture on binary trees

--- march_2023/9.3.23/BT_8.cpp
+++ march_2023/9.3.23/BT_8.cpp
@@ -67,10 +67,144 @@ void post_order(BinaryTreeNode<int> * root){
     cout<<root->data<<" ";
 }
 
+BinaryTreeNode<int>* construct(int * pre, int * in, int preS, int preE, int inS, int inE ){
+    if(inE<inS) return NULL;
+   BinaryTreeNode<int> * root = new BinaryTreeNode<int>(pre[preS]);
+   int LinS = inS;
+   int i=0;
+   while(in[i]!=root->data){
+    i++;
+   }
+   int LinE = i-1;
+   int LpreS = preS+1;
+   int LpreE = LpreS + LinE - LinS;
+   BinaryTreeNode<int> * leftChild = construct(pre,in,LpreS,LpreE,LinS,LinE);
+   root->left = leftChild;
+   int RinS = i+1;
+   int RinE = inE;
+   int RpreS = LpreE+1;
+   int RpreE = preE;
+   BinaryTreeNode<int> * rightChild = construct(pre,in,RpreS,RpreE,RinS,RinE);
+   root->right = rightChild;
+   return root;
+}
+
+BinaryTreeNode<int>* construct_2(int * pos, int * in, int posS, int posE, int inS, int inE ){
+    if(inE<inS) return NULL;
+   BinaryTreeNode<int> * root = new BinaryTreeNode<int>(pos[posE]);
+   int LinS = inS;
+   int i=0;
+   while(in[i]!=root->data){
+    i++;
+   }
+   int LinE = i-1;
+   int LposS = posS;
+   int LposE = LposS + LinE - LinS;
+   BinaryTreeNode<int> * leftChild = construct_2(pos,in,LposS,LposE,LinS,LinE);
+   root->left = leftChild;
+   int RinS = i+1;
+   int RinE = inE;
+   int RposS = LposE+1;
+   int RposE = posE-1;
+   BinaryTreeNode<int> * rightChild = construct_2(pos,in,RposS,RposE,RinS,RinE);
+   root->right = rightChild;
+   return root;
+}
+
+int height(BinaryTreeNode<int> * root){
+    if(root==NULL) return 0;
+    return max(height(root->left),height(root->right)) + 1;
+}
+
+int diameter(BinaryTreeNode<int> * root){
+    if(root==NULL) return 0;
+    int a = height(root->left) + height(root->right);
+    int b = diameter(root->left);
+    int c = diameter(root->right);
+    return max(a,max(b,c));
+}
+
+pair<int,int> diameter_height(BinaryTreeNode<int> * root){
+    if(root==NULL){
+        pair<int,int> p;
+        p.first = 0;
+        p.second = 0;
+        return p;
+    }
+    pair<int,int> leftAns = diameter_height(root->left);
+    pair<int,int> rightAns = diameter_height(root->right);
+
+    int ld = leftAns.second;
+    int rd = rightAns.second;
+    int lh = leftAns.first;
+    int rh = rightAns.first;
+
+    int overall_height = 1 + max(lh,rh);
+    int overall_diameter = max(lh+rh, max(rd,ld));
+    pair<int,int> p;
+        p.first = overall_height;
+        p.second = overall_diameter;
+        return p;
+}
+
+pair<int,int> min_max(BinaryTreeNode<int> * root){
+    if(root->left==NULL && root->right==NULL){
+        pair<int,int> p;
+        p.first = root->data;
+        p.second = -1;
+        return p;
+    }
+
+    pair<int,int> leftans = min_max(root->left);
+    pair<int,int> rightans = min_max(root->right);
+
+    pair<int,int> ans;
+    if(leftans.first>rightans.first){
+        ans.first = leftans.first;
+        if(rightans.first>leftans.second){
+        ans.second = rightans.first;
+        }
+        else{
+            ans.second = leftans.second;
+        }
+    }
+    else{
+        ans.first = rightans.first;
+        if(leftans.first>rightans.second){
+        ans.second = leftans.first;
+        }
+        else{
+            ans.second = rightans.second;
+        }
+    }
+    if(root->data>ans.fisrt){
+        
+    }
+    return ans;
+}
 
 int main()
 {   BinaryTreeNode<int> * root = takeinput_levelwise();
-    //pre_order(root);
-    //post_order(root);
+    cout<<min_max(root).first<<" "<<min_max(root).second;
+    //cout<<diameter_height(root).second;
+    // int n;
+    // cin>>n;
+    // int * pos = new int[n];
+    // for(int i=0; i<n; i++){
+    //     cin>>pos[i];
+    // }
+    // int * in = new int[n];
+    // for(int i=0; i<n; i++){
+    //     cin>>in[i];
+    // }
+    // int posS = 0;
+    // int posE = n-1;
+    // int inS = 0;
+    // int inE = n-1;
+    // BinaryTreeNode<int> * root = construct_2(pos, in, posS, posE, inS, inE);
+    // // construct(pre, in, preS, preE, inS, inE);
+    // //pre_order(root);
+    // //post_order(root);
+    // print_levelwise(root);
     return 0;
 }
