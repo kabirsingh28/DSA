2f25831712d44ff5a2c10dff0be89ea54bea753f
kabirsingh28 <7darkshades@gmail.com>
2023-04-12 17:15:39 +0530

    learned inbuild hashmaps

--- april_2023/11.4.23/inbuild_hashmaps.cpp
+++ april_2023/11.4.23/inbuild_hashmaps.cpp
@@ -1,6 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main()
-{
+{   unordered_map<string,int> myMap;
+    pair<string,int> p("kabir",28);
+    myMap.insert(p);
+    myMap["singh"] = 2;
+    cout<<myMap["kabir"]<<endl;
+    cout<<myMap.at("singh")<<endl;
+    cout<<myMap["hanspal"]<<endl;
+    cout<<myMap["ramghariya"]<<endl;
+    //cout<<myMap.at("kala-ka")<<endl;
+    cout<<myMap.count("singh")<<endl;
+    if(myMap.count("kabir")) cout<<"exsists"<<endl;
+    cout<<myMap.size();
+    myMap.erase("kabir");  
+    if(myMap.count("kabir")) cout<<"exsists"<<endl;
+
     return 0;
 }
