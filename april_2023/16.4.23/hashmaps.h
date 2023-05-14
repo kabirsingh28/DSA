2d17bc49a0cbac989bf5ef80716eac3afed86115
kabirsingh28 <7darkshades@gmail.com>
2023-05-14 21:13:39 +0530

    continuing dsa

--- april_2023/16.4.23/hashmaps.h
+++ april_2023/16.4.23/hashmaps.h
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+using namespace std;
+ 
 template <typename V>
 class mapNode{
     public:
@@ -26,7 +28,14 @@ class myMap{
 
     int getbucket_index(string key){
         int hashcode = 0;
-        current_coeff = 1;
+        int current_coeff = 1;
+        for(int i=key.size()-1; i>=0; i--){
+            hashcode += current_coeff*key[i];
+            hashcode%=num_buckets;
+            current_coeff*=37;
+            current_coeff%=num_buckets;
+        }
+        return hashcode%num_buckets;
         
      }
     public:
@@ -48,6 +57,24 @@ class myMap{
 
     void insert(string key, V value){
         int bucket_index = getbucket_index(key);
+        mapNode<V>* head = buckets[bucket_index];
+        while(head!=NULL){
+            if(head->key == key){
+                head->value = value;
+                return; 
+        }
+            head = head->next;
     }
+      head = buckets[bucket_index];
+      mapNode<V>* newNode = new mapNode<V> * (key,value);
+      newNode->next = head;
+      buckets[bucket_index] = newNode; //now array stores the address of newNode as head 
+      count++;
+    }
+    
+    V remove(string key){
+         
+    }
+
 
 };
--- april_2023/16.4.23/hashmaps6.cpp
+++ april_2023/16.4.23/hashmaps6.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
+#include "hashmaps.h"
 int main()
 {
     return 0;
